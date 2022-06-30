02b9db1620c0ebf3bfe2c6284b0acce8ce8b4d9d
oldcheese <okistheengineer@gmail.com>
2022-06-30 15:51:09 +0800

    add health check api

--- src/main/java/com/example/demo/SpringConfigServerApplication.java
+++ src/main/java/com/example/demo/SpringConfigServerApplication.java
@@ -6,14 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
+@RestController
 public class SpringConfigServerApplication {
-
+	@GetMapping("health")
+	public String health() {
+		return "ok";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConfigServerApplication.class, args);
 	}
